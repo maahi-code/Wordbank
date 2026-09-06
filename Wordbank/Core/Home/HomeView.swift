bee8a47e8db3b00bd224ccb443a51366f4cafc32
Mahipal Singh <ms4341547@gmail.com>
2026-09-06 22:18:37 +0530

    feat(Config): Added icon and brand Colors

---
+++ Wordbank/Core/Home/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  Wordbank
+//
+//  Created by Mahipal Singh on 06/09/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Wordbank!")
+            .font(.brandTitle)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Wordbank/Extensions/ViewExt.swift
@@ -0,0 +1,35 @@
+//
+//  ViewExt.swift
+//  Wordbank
+//
+//  Created by Mahipal Singh on 06/09/26.
+//
+import SwiftUI
+
+extension View {
+
+    func headword() -> some View {
+        self
+            .font(.brandWord)
+            .foregroundStyle(Color(.brandInk))
+    }
+
+    func definition() -> some View {
+        self
+            .font(.brandQuote)
+            .foregroundStyle(.secondary)
+    }
+
+    func metadataLabel() -> some View {
+        self
+            .font(.brandLabel)
+            .textCase(.uppercase)
+            .foregroundStyle(.secondary)
+    }
+
+    func brandSurface() -> some View {
+        self
+            .background(Color(.brandPaper))
+            .foregroundStyle(Color(.brandInk))
+    }
+}
