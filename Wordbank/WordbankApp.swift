0d7da4f4cc36c8ae39390e9b4e01a0416cb4f763
Mahipal Singh <ms4341547@gmail.com>
2026-09-06 20:35:52 +0530

    Initial Commit

---
+++ Wordbank/WordbankApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordbankApp.swift
+//  Wordbank
+//
+//  Created by Mahipal Singh on 06/09/26.
+//
+
+import SwiftUI
+
+@main
+struct WordbankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordbankTests/WordbankTests.swift
@@ -0,0 +1,19 @@
+//
+//  WordbankTests.swift
+//  WordbankTests
+//
+//  Created by Mahipal Singh on 06/09/26.
+//
+
+import Testing
+@testable import Wordbank
+
+struct WordbankTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
